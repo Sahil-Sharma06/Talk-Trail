76ceb7c28d643519063445da309ad248bf2dc956
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-09-03 16:23:33 +0530

    Login and logout controllers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import {User} from "../models/usermodel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
 export const register = async(req,res)=>{
     try {
         const {fullname,username,password,confirmPassword,gender} = req.body;
@@ -37,3 +38,52 @@ export const register = async(req,res)=>{
     }
 };
 
+export const login = async (req,res)=>{
+    try {
+        const {username,password} = req.body;
+        if(!username || !password){
+            return res.status(400).json({message:"All fields are required."});
+        };
+        const user = await User.findOne({username});
+        if(!user){
+            return res.statis(400).json({
+                message:"Incorrect username!",
+                success:false
+            })
+        };
+
+        const isPasswordMatched = await bcrypt.compare(password,user.password);
+        if(!isPasswordMatched){
+            return res.status(400).json({
+                message:"Invalid Password!",
+                success:false
+            })
+        }
+
+        const tokenData = {
+            userId:user._id
+        }
+
+        const token = await jwt.sign(tokenData,process.env.JWT_SECRET_KEY, {expiresIn:'1d'});
+
+        return res.status(200).cookie("token",token,{maxAge:1*24*60*60*1000, httpOnly:true, sameSite:'strict'}).json({
+            _id:user._id,
+            username:user.username,
+            fullname:user.fullname,
+            profilePhoto:user.profilePhoto
+        });
+    } catch (error) {
+        console.log(error);
+        
+    }
+};
+
+export const logout = (req,res)=>{
+    try {
+        return res.status(200).cookie("token","",{maxAge:0}).json({
+            message:"Logged out successfully"
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,11 @@
 import express from "express";
-import {register} from "../controllers/userController.js"
+import {register, login, logout} from "../controllers/userController.js"
+
 
 const router = express.Router();
 
 router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/logout").get(logout);
 
 export default router;
