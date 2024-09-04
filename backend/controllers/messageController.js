64c816f80ac815f37e386f7d157572a5e88d039e
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-09-04 15:14:22 +0530

    Message Conroller

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,7 +1,11 @@
 export const sendMessage = async (req,res)=>{
     try {
-        
+        const senderId=req.id;
+        const receiverId=req.params.id;
+        const {message} = req.body;
+        let gotConversation =  await Conversation.findOne({})
     } catch (error) {
+        console.log(error);
         
     }
 }
