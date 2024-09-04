178abb89a83f10f4c91eba92793be9e8e7e4101e
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-09-04 15:48:28 +0530

    Message controller and message route

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -1,9 +1,35 @@
+import {Conversation} from "../models/conversationModel.js"
 export const sendMessage = async (req,res)=>{
     try {
         const senderId=req.id;
         const receiverId=req.params.id;
         const {message} = req.body;
-        let gotConversation =  await Conversation.findOne({})
+        let gotConversation =  await Conversation.findOne({
+            participants:{$all:[senderId,receiverId]},
+        });
+
+        if(!gotConversation){
+            gotConversation = await Conversation.create({
+                participants:[senderId,receiverId]
+            })
+        };
+
+        const newMessage = await Message.create({
+            senderId,
+            receiverId,
+            message
+        });
+
+        if(newMessage){
+            gotConversation.messages.push(newMessage._id);
+        };
+
+        await gotConversation.save();
+
+        return res.status(201).json({
+            message:"Message sent successfully",
+            success:true,
+        });
     } catch (error) {
         console.log(error);
         
--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -1,8 +1,9 @@
 import express from "express"
 import {sendMessage} from "../controllers/messageController.js"
+import isAuthenticated from "../middleware/isAuthenticated.js"
 
 const router = express.Router();
 
-router.route("/send/:id").post(sendMessage);
+router.route("/send/:id").post(isAuthenticated,sendMessage);
 
 export default router;
