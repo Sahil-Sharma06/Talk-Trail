ab9e9f06315f65bb4a8d45bfce2c6107a64ffde6
Sahil-Sharma06 <116885079+Sahil-Sharma06@users.noreply.github.com>
2024-09-08 02:30:49 +0530

    New Components

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Homepage = () => {
   return (
-    <div>
-      Home
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+      <Sidebar />
+      <MessageContainer />
     </div>
   )
 }
